d472f47f97db43eaf16f3b3601f5470515dfecc7
Subramaniam Padmanabhan <subramaniam_p@apple.com>
2023-07-09 10:56:14 -0400

    first commit

---
+++ TODO.Services.ToDoAPI/Migrations/20230709140111_ToDoDatabase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TODO.Services.ToDoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDoDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    ToDoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Task = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Done = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.ToDoId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "ToDoId", "Done", "DueDate", "Task" },
+                values: new object[,]
+                {
+                    { 1, false, new DateTime(2023, 7, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Get Groceries" },
+                    { 2, false, new DateTime(2023, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Pay rent" },
+                    { 3, false, new DateTime(2023, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Organize garage" },
+                    { 4, false, new DateTime(2023, 7, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Buy furniture" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
---
+++ TODO.Services.ToDoAPI/Models/Dto/ToDoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TODO.Services.ToDoAPI.Models.Dto
+{
+	public class ToDoDto
+	{		
+        public int ToDoId { get; set; }        
+        public string? Task { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Done { get; set; }    
+	}
+}
+
